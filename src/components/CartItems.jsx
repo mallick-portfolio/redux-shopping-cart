a50494c15805666f55db476b50bf8a4cd4cdcb47
mallick-portfolioE <tamal1714019@gmail.com>
2022-06-26 21:19:36 +0600

    added cart

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const CartItems = () => {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  )
-}
+    <div className="w-full bg-[#e4dfdf] rounded-md ml-2 px-2 pt-5 h-[calc(100%_-_10rem)] mb-12">
+      <h4>List of items you have selected</h4>
 
-export default CartItems
+      <div className="mt-4">
+        <table className="w-full px-4 text-sm text-left">
+          <thead className="text-xs uppercase">
+            <tr className="py-1">
+              <th >
+                Sr No
+              </th>
+              <th >
+                Item Name
+              </th>
+              <th >
+                Qty
+              </th>
+              <th >
+                Price
+              </th>
+              <th >
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td
+              
+              >
+                Apple MacBook Pro 17"
+              </td>
+              <td>Sliver</td>
+              <td>Laptop</td>
+              <td>$2999</td>
+              <td className="text-xl">x</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default CartItems;
