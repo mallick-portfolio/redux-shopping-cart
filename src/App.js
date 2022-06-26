187edbb893bbdcc4dd9922c09153e90961873156
mallick-portfolioE <tamal1714019@gmail.com>
2022-06-26 20:49:52 +0600

    added product

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import CartItems from './components/CartItems.jsx';
-import Products from './components/Products.jsx';
+import "./App.css";
+import CartItems from "./components/CartItems.jsx";
+import Products from "./components/Products.jsx";
 
 function App() {
   return (
-    <div className="flex justify-between container px-12 mx-auto mt-10">
-      <Products />
-      <CartItems />
+    <div className="flex justify-between container px-2  mx-auto mt-10">
+      <div className="w-4/6">
+        <Products />
+      </div>
+      <div className="w-2/6">
+        <CartItems />
+      </div>
     </div>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,131 @@
-import React from 'react'
-import pizza from '../../src/assets/images/piz1.jpg'
+import React from "react";
+import productImg from "../assets/images/150.png";
 const Products = () => {
   return (
-    <div className="">
-      <div className="flex">
-        <div>
-          <div className="w-24 h-24 bg-red-500"></div>
+    <div className="grid grid-cols-2 gap-5 h-[calc(100%_-_10rem)] overflow-y-auto product-scroll pr-4">
+      <div className="flex px-2 py-6 justify-between items-end bg-[#e4dfdf] rounded-md">
+        <div className="flex items-center w-full">
+          <img className="w-28" src={productImg} alt="" />
+          <div className="ml-4">
+            <h3 className="text-3xl mb-2">Wrapsafe1</h3>
+            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur</p>
+          </div>
         </div>
-        <div>
-          
+        <div className="w-auto ml-1">
+          <button className="px-2 py-1 mb-2 rounded-md bg-[#ccc] text-xs">
+            Add to list
+          </button>
+          <button className="px-2 py-1 rounded-md bg-[#ccc] text-xs">
+            Details
+          </button>
         </div>
       </div>
+      <div className="flex px-2 py-6 justify-between items-end bg-[#e4dfdf] rounded-md">
+        <div className="flex items-center w-full">
+          <img className="w-28" src={productImg} alt="" />
+          <div className="ml-4">
+            <h3 className="text-3xl mb-2">Wrapsafe1</h3>
+            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur</p>
+          </div>
+        </div>
+        <div className="w-auto ml-1">
+          <button className="px-2 py-1 mb-2 rounded-md bg-[#ccc] text-xs">
+            Add to list
+          </button>
+          <button className="px-2 py-1 rounded-md bg-[#ccc] text-xs">
+            Details
+          </button>
+        </div>
+      </div>
+      <div className="flex px-2 py-6 justify-between items-end bg-[#e4dfdf] rounded-md">
+        <div className="flex items-center w-full">
+          <img className="w-28" src={productImg} alt="" />
+          <div className="ml-4">
+            <h3 className="text-3xl mb-2">Wrapsafe1</h3>
+            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur</p>
+          </div>
+        </div>
+        <div className="w-auto ml-1">
+          <button className="px-2 py-1 mb-2 rounded-md bg-[#ccc] text-xs">
+            Add to list
+          </button>
+          <button className="px-2 py-1 rounded-md bg-[#ccc] text-xs">
+            Details
+          </button>
+        </div>
+      </div>
+      <div className="flex px-2 py-6 justify-between items-end bg-[#e4dfdf] rounded-md">
+        <div className="flex items-center w-full">
+          <img className="w-28" src={productImg} alt="" />
+          <div className="ml-4">
+            <h3 className="text-3xl mb-2">Wrapsafe1</h3>
+            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur</p>
+          </div>
+        </div>
+        <div className="w-auto ml-1">
+          <button className="px-2 py-1 mb-2 rounded-md bg-[#ccc] text-xs">
+            Add to list
+          </button>
+          <button className="px-2 py-1 rounded-md bg-[#ccc] text-xs">
+            Details
+          </button>
+        </div>
+      </div>
+      <div className="flex px-2 py-6 justify-between items-end bg-[#e4dfdf] rounded-md">
+        <div className="flex items-center w-full">
+          <img className="w-28" src={productImg} alt="" />
+          <div className="ml-4">
+            <h3 className="text-3xl mb-2">Wrapsafe1</h3>
+            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur</p>
+          </div>
+        </div>
+        <div className="w-auto ml-1">
+          <button className="px-2 py-1 mb-2 rounded-md bg-[#ccc] text-xs">
+            Add to list
+          </button>
+          <button className="px-2 py-1 rounded-md bg-[#ccc] text-xs">
+            Details
+          </button>
+        </div>
+      </div>
+      <div className="flex px-2 py-6 justify-between items-end bg-[#e4dfdf] rounded-md">
+        <div className="flex items-center w-full">
+          <img className="w-28" src={productImg} alt="" />
+          <div className="ml-4">
+            <h3 className="text-3xl mb-2">Wrapsafe1</h3>
+            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur</p>
+          </div>
+        </div>
+        <div className="w-auto ml-1">
+          <button className="px-2 py-1 mb-2 rounded-md bg-[#ccc] text-xs">
+            Add to list
+          </button>
+          <button className="px-2 py-1 rounded-md bg-[#ccc] text-xs">
+            Details
+          </button>
+        </div>
+      </div>
+      <div className="flex px-2 py-6 justify-between items-end bg-[#e4dfdf] rounded-md">
+        <div className="flex items-center w-full">
+          <img className="w-28" src={productImg} alt="" />
+          <div className="ml-4">
+            <h3 className="text-3xl mb-2">Wrapsafe1</h3>
+            <p className="text-sm">Lorem ipsum dolor sit, amet consectetur</p>
+          </div>
+        </div>
+        <div className="w-auto ml-1">
+          <button className="px-2 py-1 mb-2 rounded-md bg-[#ccc] text-xs">
+            Add to list
+          </button>
+          <button className="px-2 py-1 rounded-md bg-[#ccc] text-xs">
+            Details
+          </button>
+        </div>
+      </div>
+      
+      
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
