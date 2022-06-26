e1643e8ff88dbf966545347415fdfca6e792996a
mallick-portfolioE <tamal1714019@gmail.com>
2022-06-26 20:08:42 +0600

    start cart design

---
+++ src/components/Products.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import pizza from '../../src/assets/images/piz1.jpg'
+const Products = () => {
+  return (
+    <div className="">
+      <div className="flex">
+        <div>
+          <div className="w-24 h-24 bg-red-500"></div>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Products
